2408787c85cbc853b90f6b8db737af9a0478a8c4
stephen <liluzisteveee@gmail.com>
2022-05-06 01:50:32 -0400

    finished logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const p1 = document.getElementById("createquiz");
 const p = document.getElementById("makequiz");
 const num = document.getElementById("numquestions"); 
 const form = document.getElementById("questions");
+const formquestions = document.getElementById("addquestions");
 function create() {
     p.style.display = "none";
     p1.style.display = "block";
@@ -13,7 +14,7 @@ function begin() {
     if(value.length > 0 && value != 0) {
         document.getElementById("begin").disabled = false;
         p1.style.display = "none";
-        form.style.display ="block";
+        form.style.display = "block";
     }
     else {
         document.getElementById("begin").disabled = true;
@@ -23,17 +24,24 @@ function begin() {
 const questions = [];
 const correct = [];
 const wrong = [];
-
+let count = 0;
 function save() {
     const form = document.getElementById("questions");
     let question = document.getElementById("question").value;
     questions.push(question);
     let correctAns = document.getElementById("correctanswer").value;
     correct.push(correctAns);
-    let getWrong = form.querySelector('input[name="wrong"]');
-    let wrongArr = Array.from(getWrong);
+    const wrongList = form.querySelectorAll('input[name="wrong"]');
+    const wrongArr = []
+    for(let i = 0; i < wrongList.length; i++) {
+        wrongArr.push(wrongList[i].value);
+    }
     wrong.push(wrongArr);
-    console.log(correct);
-    form.reset();
-
+    count = count + 1;
+    if(count == num.value) {
+        form.style.display = "none";
+    }
+    else {
+        form.reset();
+    }
 }
